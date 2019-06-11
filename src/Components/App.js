87a9e12dc82166d3c10f32c7d69622a7740e7d2e
Kim Schneider <hi@schneider.kim>
2019-06-11 20:24:10 +0200

    list books and bookdetails

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,8 +10,13 @@ export default class App extends Component {
     super(props);
 
     this.state = {
+      bookSelected: false,
       books: [],
     };
+  }
+
+  componentDidMount() {
+    this.getBookShelves();
   }
 
   getBooks = subject => {
@@ -19,14 +24,39 @@ export default class App extends Component {
     books.then(books => {
       this.setState({
         books: books,
+        activeBook: {},
+        bookSelected: false,
       });
     });
   };
+
+  getBookDetails = book => {
+    this.setState({
+      activeBook: this.state.books[book],
+      bookSelected: true,
+    });
+  };
+
+  getBookShelves = () => {
+    let bookShelves = endpoint.getBookShelves();
+    bookShelves.then(bookShelve => {
+      this.setState({
+        bookShelves: bookShelve.flat(),
+      });
+    });
+  };
+
   render() {
     return (
       <div className="appFrame">
         <Subjects searchBooks={this.getBooks} />
-        <Books books={this.state.books} />
+        <Books books={this.state.books} getBookDetails={this.getBookDetails} />
+        <BookDetails
+          bookSelected={this.state.bookSelected}
+          activeBook={this.state.activeBook}
+          saveBook={this.updateActiveBook}
+          bookShelves={this.state.bookShelves}
+        />
       </div>
     );
   }
