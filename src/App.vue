9d6b9242d475903ba037b81a154a7c2dbec88a2a
Raimisjes <raimisjes@gmail.com>
2023-02-17 15:39:39 +0200

    vue book info edit app

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <books-form />
+  </div>
+</template>
+
+<script>
+import BooksForm from './components/BooksForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BooksForm
+  }
+}
+</script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createPinia } from 'pinia'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
